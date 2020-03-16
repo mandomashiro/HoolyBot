aec1eb01c6a97b16ed1e7255cd29261377ffbdb5
mandomashiro <mandojeebot@gmail.com>
2020-03-16 20:54:34 +0100

    initialisation du projet

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "Njg3MzMxODMyOTI4MTQxMzY2.Xm_R0w.8IG8TD4ffGz4rOuYBG183N4aoVo",
+  TOKEN: "Njg3MzMxODMyOTI4MTQxMzY2.Xm_Xqg.D3STnRZRl2Z8oir8U0YsvLksF9g",
   PREFIX: "!",
   DBCONNECTION: "mongodb://localhost:27017/test",
   DEFAULTSETTINGS: {
